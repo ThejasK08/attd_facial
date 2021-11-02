c189657a22413c96b647abe7ebe6a10a3bc83924
ThejasK08 <thejask8@gmail.com>
2021-11-02 14:24:57 +0530

    Add files via upload

---
+++ opencv-face-recognition/recognize_video.py
@@ -0,0 +1,206 @@
+# USAGE
+# python recognize_video.py --detector face_detection_model \
+#	--embedding-model openface_nn4.small2.v1.t7 \
+#	--recognizer output/recognizer.pickle \
+#	--le output/le.pickle
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime
+import numpy as np
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+
+try:
+    proba=a1=a2=a3=0
+    connection = mysql.connector.connect(host='127.0.0.1',
+                             database='attendance',
+                             user='root',
+                             password='')
+    if connection.is_connected():
+       db_Info = connection.get_server_info()
+       print("Connected to MySQL database... MySQL Server version on ",db_Info)
+       cursor = connection.cursor()
+    f1 = open("duration.txt","r+")
+    
+    while(os.path.getsize(r"C:\Users\USER\opencv-face-recognition\duration.txt") <= 0):
+    	print("waiting for intiating session....");
+    	time.sleep(5.0)
+    cid=f1.readline();
+    print(cid);
+    cid_int=cid.rstrip('\n');#int(cid)
+    dur = int(f1.readline(),10);
+    print("\ncourse id : ",cid);
+    print("\nDuration of session : ",dur);
+    
+    f1.truncate(0);
+    f1.close;    
+        # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--detector", required=True,
+    	help="path to OpenCV's deep learning face detector")
+    ap.add_argument("-m", "--embedding-model", required=True,
+    	help="path to OpenCV's deep learning face embedding model")
+    ap.add_argument("-r", "--recognizer", required=True,
+    	help="path to model trained to recognize faces")
+    ap.add_argument("-l", "--le", required=True,
+    	help="path to label encoder")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    	help="minimum probability to filter weak detections")
+    args = vars(ap.parse_args())
+    
+    # load our serialized face detector from disk
+    print("[INFO] loading face detector...")
+    protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+    modelPath = os.path.sep.join([args["detector"],
+    	"res10_300x300_ssd_iter_140000.caffemodel"])
+    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+    
+    # load our serialized face embedding model from disk
+    print("[INFO] loading face recognizer...")
+    embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+    
+    # load the actual face recognition model along with the label encoder
+    recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+    le = pickle.loads(open(args["le"], "rb").read())
+    
+    # initialize the video stream, then allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+    
+    # start the FPS throughput estimator
+    fps = FPS().start()
+    list=[]
+    start_time = datetime.now()
+    
+    # loop over frames from the video file stream
+    while True:
+    	# grab the frame from the threaded video stream
+    	frame = vs.read()
+    
+    	# resize the frame to have a width of 600 pixels (while
+    	# maintaining the aspect ratio), and then grab the image
+    	# dimensions
+    	frame = imutils.resize(frame, width=600)
+    	(h, w) = frame.shape[:2]
+    
+    	# construct a blob from the image
+    	imageBlob = cv2.dnn.blobFromImage(
+    		cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+    		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+    
+    	# apply OpenCV's deep learning-based face detector to localize
+    	# faces in the input image
+    	detector.setInput(imageBlob)
+    	detections = detector.forward()
+    
+    	# loop over the detections
+    	for i in range(0, detections.shape[2]):
+    		# extract the confidence (i.e., probability) associated with
+    		# the prediction
+    		confidence = detections[0, 0, i, 2]
+    
+    		# filter out weak detections
+    		if confidence > args["confidence"]:
+    			# compute the (x, y)-coordinates of the bounding box for
+    			# the face
+    			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    			(startX, startY, endX, endY) = box.astype("int")
+    
+    			# extract the face ROI
+    			face = frame[startY:endY, startX:endX]
+    			(fH, fW) = face.shape[:2]
+    
+    			# ensure the face width and height are sufficiently large
+    			if fW < 20 or fH < 20:
+    				continue
+    
+    			# construct a blob for the face ROI, then pass the blob
+    			# through our face embedding model to obtain the 128-d
+    			# quantification of the face
+    			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+    				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+    			embedder.setInput(faceBlob)
+    			vec = embedder.forward()
+    
+    			# perform classification to recognize the face
+    			preds = recognizer.predict_proba(vec)[0]
+    			j = np.argmax(preds)
+    			proba = preds[j]
+    			name = le.classes_[j]
+    
+    			# draw the bounding box of the face along with the
+    			# associated probability
+    			text = "{}: {:.2f}%".format(name, proba * 100)
+    			y = startY - 10 if startY - 10 > 10 else startY + 10
+    			cv2.rectangle(frame, (startX, startY), (endX, endY),
+    				(0, 0, 255), 2)
+    			cv2.putText(frame, text, (startX, y),
+    				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    
+    	# update the FPS counter
+    	fps.update()
+    	# show the output frame
+    	cv2.imshow("Frame", frame)
+    	key = cv2.waitKey(1) & 0xFF
+    	check = proba*100    
+    	# if the `x` key was pressed, break from the loop
+    	if check >= 72:
+    		fo = open("foo.txt","w")
+    		if name not in list:    		
+    				list.append(name)
+    		#print(list)
+    			                
+    		#break
+    		time_delta = datetime.now() - start_time
+    		if time_delta.total_seconds() >= 5:
+    				break
+    
+    # stop the timer and display FPS information
+    fps.stop()
+    for item in list:
+    	fo.write("%s\n" % item)
+    fo.close;
+    img_item = "spi.jpg"
+    cv2.imwrite(img_item, face)
+    print(list)
+    print(check)
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+    print(cid);
+    for item in list:
+        print(item);
+        q="SELECT %s FROM attend where sid= '%s' " % (cid_int,item)
+        cursor.execute(q)
+        records = cursor.fetchall();
+        print(records);
+        for row in records:
+            print(row);
+        print(row[0]);
+        val=int(str(row[0]),10);
+        dur = dur+val;
+        print("\nUpdated Attendance",dur);
+        q="UPDATE `attend` SET %s=%d WHERE sid ='%s'" % (cid_int,dur,item)
+        cursor.execute(q)
+           
+except Error as e :
+    print ("Error while connecting to MySQL", e)
+finally:
+    #closing database connection.
+    if(connection.is_connected()):
+        cursor.close()
+        connection.commit()
+        connection.close()
+        print("MySQL connection is closed")
